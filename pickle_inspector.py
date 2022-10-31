b2eddf16c1271996e2b3744ef7b41e09d664434e
lopho <lopho@users.noreply.github.com>
2022-10-31 07:01:20 +0100

    record parameters in stubs
    Signed-off-by: lopho <lopho@users.noreply.github.com>

--- pickle_inspector.py
+++ pickle_inspector.py
@@ -2,8 +2,11 @@ import pickle as python_pickle
 from types import ModuleType
 
 class Stub:
-    def __setstate__(*_): pass
-    def __init__(*_): pass
+    def __setstate__(self, *p):
+        self.payload_state = p
+    def __init__(self, *p):
+        self.payload_init = p
+        self.payload_state = ()
 
 class UnpickleInspector(python_pickle.Unpickler):
     def find_class(self, module, name):
