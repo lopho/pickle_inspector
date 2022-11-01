4ac141442fd8f11caab23c010af812cdc8fcf961
lopho <lopho@users.noreply.github.com>
2022-11-01 03:37:46 +0100

    record classes in list instead of printing
    Signed-off-by: lopho <lopho@users.noreply.github.com>

--- pickle_inspector.py
+++ pickle_inspector.py
@@ -6,13 +6,14 @@ from functools import partial
 
 class InspectorResult:
     def __init__(self):
+        self.classes = []
         self.calls = []
         self.structure = {}
 
 
 class UnpickleInspector(python_pickle.Unpickler):
-    def find_class(self, calls, module, name):
-        print(f'{module}.{name}')
+    def find_class(self, result, module, name):
+        result.classes.append(f'{module}.{name}')
         class Stub:
             def __init__(self, *args, **kwargs):
                 self.module = module
@@ -21,11 +22,11 @@ class UnpickleInspector(python_pickle.Unpickler):
                 self.init_kwargs = kwargs
                 self.state_args = ()
                 self.state_kwargs = {}
-                calls.append(f'{module}.{name}({args}, {kwargs})')
+                result.calls.append(f'{module}.{name}({args}, {kwargs})')
             def __setstate__(self, *args, **kwargs):
                 self.state_args = args
                 self.state_kwargs = kwargs
-                calls.append(f'{module}.{name}.__setstate__({args}, {kwargs})')
+                result.calls.append(f'{module}.{name}.__setstate__({args}, {kwargs})')
             def __repr__(self):
                 return f'{self.module}.{self.name}({self.init_args}, {self.init_kwargs})'
         return Stub
@@ -35,7 +36,7 @@ class UnpickleInspector(python_pickle.Unpickler):
 
     def load(self):
         result = InspectorResult()
-        self.find_class = partial(UnpickleInspector.find_class, self, result.calls)
+        self.find_class = partial(UnpickleInspector.find_class, self, result)
         result.structure = super().load()
         return result
 
